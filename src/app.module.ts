22b60ceee808a3b4284bcf46c7f079dcb462551f
tao.lai <tao.lai@yintech.cn>
2022-06-06 14:42:52 +0800

    feat-清理

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
-// import { TypeOrmModule } from '@nestjs/typeorm';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [UserModule],
+  imports: [UserModule, TypeOrmModule.forRoot()],
   controllers: [AppController],
   providers: [AppService],
 })
